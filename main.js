7f4e422716471d4eb6f3429206b59ff12bd00dcf
ricardosoaresds <rihcardosoares@gmail.com>
2022-03-25 16:40:25 -0300

    Inserindo JS

---
+++ main.js
@@ -0,0 +1,12 @@
+/* Abre e fecho menu quando clicar no ícone: hamburguer e x */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item do menu, esconder o menu */
